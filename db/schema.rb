b7d799de6a7b0d496d2edc5f537d05f0c24392af
ewhitebloom <ewhitebloom@gmail.com>
2014-03-19 11:35:22 -0400

    categorizations table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140319151535) do
+ActiveRecord::Schema.define(version: 20140319152041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,11 @@ ActiveRecord::Schema.define(version: 20140319151535) do
   create_table "checkouts", force: true do |t|
     t.integer  "book_id",    null: false
     t.string   "user_name",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "readers", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
